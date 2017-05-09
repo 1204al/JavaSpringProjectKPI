732c6f10b13db086409f9eeae61bf035d52f8f45
Alona Vit <Alona Vit>
2017-05-09 18:26:02 +0300

    Spring Boot project generated

---
+++ src/main/java/com/kpicoursework/GlossaryApplication.java
@@ -0,0 +1,12 @@
+package com.kpicoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossaryApplication.class, args);
+	}
+}
