202b0873ea378aa74765676ff3e9f9c9845f4cbd
aliubivyi <120494al@gmail.com>
2017-06-04 15:12:55 +0300

    count of word

--- src/main/java/com/glossary/mvc/TextUploadController.java
+++ src/main/java/com/glossary/mvc/TextUploadController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by aliubivyi on 31.05.17.
@@ -21,7 +19,41 @@ public class TextUploadController {
     String handleTextUpload(@RequestParam("text") String text) {
         String[] parts = (text + " ").split("\\p{P}?[ \\t\\n\\r]+");
         List<String> words=Arrays.asList(parts);
-        System.out.println(words);
+        TreeMap<String, Integer> wordsFrequency = new TreeMap<String, Integer>();  // key:words; value:times the word comes across in the book
+        for (int i = 0; i < words.size(); i++) {
+            String s=words.get(i).toLowerCase();
+            if(wordsFrequency.containsKey(s)){
+                wordsFrequency.put(s,wordsFrequency.get(s)+1);
+            }else{
+                wordsFrequency.put(s,1+1);
+            }
+        }
+        class WordFrequency implements Comparable{
+            public WordFrequency(String word, int frequency) {
+                this.word = word;
+                this.frequency = frequency;
+            }
+            public String word;
+            public int frequency;
+            @Override
+            public int compareTo(Object o) {
+                return this.frequency-((WordFrequency) o).frequency;
+            }
+        }
+
+        List<WordFrequency> listOfWordFrequencies=new ArrayList<WordFrequency>();
+        for (Map.Entry<String,Integer> entry:wordsFrequency.entrySet()) {
+            String word=entry.getKey();
+            Integer frequency=entry.getValue();
+            listOfWordFrequencies.add(new WordFrequency(word,frequency));
+        }
+        List<String> listOfWords=new ArrayList<>();
+        for (int i = 0; i < listOfWordFrequencies.size(); i++) {
+            listOfWords.add(listOfWordFrequencies.get(i).word);
+        }
+        System.out.println(listOfWords);
+
+
 
 
         return "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCc";
